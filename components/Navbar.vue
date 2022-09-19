467345e6deee2a05b1089004764b364c4e0fe23f
yoav <yoavdan.m@gmail.com>
2022-09-19 20:14:25 +0300

    added start of navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,12 +4,27 @@
 
 
 <template>
-    <div class="header">
-        <h2>hii</h2>
-    </div>
+	<div class="navbar">
+		<div class="container">
+			<div class="logo">Y.M</div>
+		</div>
+	</div>
 </template>
 
 
 <style lang='scss'>
+.navbar {
+	@apply fixed top-0 left-0 w-full h-16;
+	@apply z-50;
 
+	.container {
+		@apply w-full h-full max-w-screen-xl m-auto;
+		@apply flex items-center px-5;
+	}
+
+	.logo {
+		font-family: "Nanum Brush Script";
+		@apply text-xl text-black bg-lime-400 p-1;
+	}
+}
 </style>

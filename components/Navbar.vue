0d231aa00bc25b1c381b6c7ff607b0a784615baf
yoav <yoavdan.m@gmail.com>
2022-09-19 20:50:34 +0300

    added nav links

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,6 +7,17 @@
 	<div class="navbar">
 		<div class="container">
 			<div class="logo">Y.M</div>
+			<div class="nav-links">
+				<li class="nav-link">
+					<nuxt-link to="/about"> ABOUT </nuxt-link>
+				</li>
+				<li class="nav-link">
+					<nuxt-link to="/projects"> PROJECTS </nuxt-link>
+				</li>
+				<li class="nav-link">
+					<nuxt-link to="/about"> CONTACT </nuxt-link>
+				</li>
+			</div>
 		</div>
 	</div>
 </template>
@@ -19,12 +30,26 @@
 
 	.container {
 		@apply w-full h-full max-w-screen-xl m-auto;
-		@apply flex items-center px-5;
+		@apply flex justify-between items-center px-5;
 	}
 
 	.logo {
 		font-family: "Nanum Brush Script";
 		@apply text-xl text-black bg-lime-400 p-1;
+	}
+
+	.nav-links {
+		@apply sm:flex gap-4 px-2 hidden;
+
+		.nav-link {
+			@apply text-lime-300  px-4 py-2 rounded-md;
+			transition: all ease 0.2s;
+			font-weight: 600;
+
+			&:hover {
+				@apply bg-opacity-25 bg-lime-300;
+			}
+		}
 	}
 }
 </style>

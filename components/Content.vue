60127e7f9d2191ade797c48d1cde78db5a2268cb
yoav <yoavdan.m@gmail.com>
2022-09-21 19:24:37 +0300

    added contanct

--- components/Content.vue
+++ components/Content.vue
@@ -4,59 +4,69 @@
 
 
 <template>
-    <section class="about content" id="about">
-        <h2>About.</h2>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis
-            explicabo voluptate libero maiores voluptas ducimus ipsam tempore
-            eos suscipit et. Reprehenderit quidem ducimus asperiores unde
-            repellendus, placeat vitae reiciendis adipisci magni suscipit
-            dolorum corrupti atque debitis. Quae facilis, provident numquam
-            similique sint fugit ducimus dolorem, delectus nisi, nesciunt nam
-            laborum odio quis? Cupiditate explicabo necessitatibus quibusdam
-            dolorem soluta, totam numquam?
-        </p>
+	<section class="about content" id="about">
+		<h2>About.</h2>
+		<p>
+			Hey, I am Yoav Mendelson, a passionate Frontend/FullStack developer.
+			initially I started Coding out of pure curiosity, it became a hobby
+			and after a while I decided to take the next step and go for it. I’m
+			a resent graduate from the
+			<a href="https://www.coding-academy.org/index.php">
+				Coding Academy
+			</a>
+			Bootcamp Where I created some of
+			<a href="#projects"> my projects </a>. Currency I’m searching for
+			the an opportunity where i could make an impact. Be sure to
+			<a href="#contact"> contact </a> me if you have any questions of
+			just wanna talk :)
+		</p>
 
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            ratione commodi ut nesciunt itaque illo deserunt id aliquam rerum,
-            quas excepturi aliquid dolorem quidem, fugit ipsum, sit nihil
-            repudiandae reiciendis eum laborum possimus veniam eos ipsam quia?
-            Rerum, totam possimus. Aliquam, minus. Pariatur debitis excepturi
-            dicta molestiae ipsa velit ut!
-        </p>
-    </section>
+		<p>
+			I have a genuine passion for development and design, i like building
+			new things that push me out of my comfort zone and challenge my
+			creativity. I'm eager to learn whenever i can and happy to embrace
+			new ideas. As a developer, my goal is make beautiful website that
+			not only look good, but also function in multiple levels. I would
+			love to make your digital vision into reality
+		</p>
+	</section>
 
-    <section class="skills content">
-        <h2 class="skills-title">Skills</h2>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo optio,
-            dolore quae perspiciatis recusandae dolores hic laboriosam tempora
-            cumque numquam sapiente, quasi nisi illum nemo vero molestias
-            repudiandae. Fuga qui placeat totam odio quia in quaerat nobis
-            quisquam ipsam voluptate?
-        </p>
-    </section>
+	<section class="skills content">
+		<h2 class="skills-title">Skills</h2>
+		<p>
+			I have experience in Vue, Javascript, Typescript, CSS/SASS, HTML,
+			MongoDB, Node.js, Tailwind, Astro , React, and more…  I prefer to
+			learn skills as I need them, so I can focus on finding solutions to
+			problems instead of trying to implement new technologies for the
+			sake of implementing them.
+		</p>
+	</section>
 </template>
 
 
 <style lang='scss'>
 .content {
-    h2 {
-        @apply text-3xl font-900 my-6;
-        // font-family: "Nanum Brush Script";
-    }
-    p {
-        @apply mb-12;
-        @apply text-base sm:text-xl leading-relaxed text-lime-50;
-        font-weight: 300;
-    }
+	h2 {
+		@apply text-3xl font-900 my-6;
+		// font-family: "Nanum Brush Script";
+	}
+	p {
+		@apply mb-12;
+		@apply text-base sm:text-xl leading-relaxed text-lime-50;
+		font-weight: 300;
+	}
 }
 
 .skills {
-    @apply mb-16;
-    .skills-title {
-        @apply font-700 text-2xl;
-    }
+	@apply mb-16;
+	.skills-title {
+		@apply font-700 text-2xl;
+	}
+}
+
+.about {
+	a {
+		@apply text-primary underline underline-offset-4 font-500;
+	}
 }
 </style>

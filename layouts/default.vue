7ef770f3d9d81148f67b2dee43262922db476948
yoav <yoavdan.m@gmail.com>
2022-09-26 18:13:39 +0300

    nuxt config seo add

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,26 +4,7 @@ import Footer from "~~/components/Footer.vue";
 
 export default {
 	head: {
-		title: "Yoav Mendelson | Web developer",
-		meta: [
-			{
-				hid: "description",
-				name: "description",
-				content:
-					"A Web Developer and Designer based in Haifa, Israel, with an eye for great design (UI) and user experience (UX).",
-			},
-			{
-				hid: "keywords",
-				name: "keywords",
-				content:
-					"Yoav Mendelson, web dev, UX/UI, software developer, design".split(
-						","
-					),
-			},
-		],
-		link: [
-			{ rel: "icon", type: "image/x-icon", href: "../public/icon.png" },
-		],
+		title: "Yoav Mendelson | Web Developer ",
 	},
 };
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,30 @@
+import { title } from "process";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
      head: {
-    title: 'my website title',
+    title: 'Yoav Mendelson | Web Developer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'my website description'
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+				hid: "description",
+				name: "description",
+				content:
+					"A Web Developer and Designer based in Haifa, Israel, with an eye for great design (UI) and user experience (UX).",
+			},
+			{
+				hid: "keywords",
+				name: "keywords",
+				content:
+					"Yoav Mendelson, web dev, UX/UI, software developer, design".split(
+						","
+					),
+			},
+		],
+		link: [
+			{ rel: "icon", type: "image/x-icon", href: "/icon.png" },
+		],
   },
 
      css: [

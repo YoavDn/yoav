034f865c6c18dae2a2b39264e24bef86f17c45cd
yoav <yoavdan.m@gmail.com>
2022-09-26 18:00:47 +0300

    added some seo meta tags

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,34 @@
-<script setup lang="ts">
+<script >
 import Navbar from "~~/components/Navbar.vue";
 import Footer from "~~/components/Footer.vue";
+
+export default {
+	head: {
+		title: "Yoav Mendelson | Web developer",
+		meta: [
+			{
+				hid: "description",
+				name: "description",
+				content:
+					"A Web Developer and Designer based in Haifa, Israel, with an eye for great design (UI) and user experience (UX).",
+			},
+			{
+				hid: "keywords",
+				name: "keywords",
+				content:
+					"Yoav Mendelson, web dev, UX/UI, software developer, design".split(
+						","
+					),
+			},
+		],
+		link: [
+			{ rel: "icon", type: "image/x-icon", href: "../public/icon.png" },
+		],
+	},
+};
 </script>
 <template>
-    <Navbar />
-    <slot />
-    <Footer />
+	<Navbar />
+	<slot />
+	<Footer />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
-import tailwindTypography from '@tailwindcss/typography';
-import postcssConfig from './postcss.config.js'
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+     head: {
+    title: 'my website title',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description'
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
 
      css: [
         '@/assets/styles/global.scss',

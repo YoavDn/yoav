719b9933e947dee4bb90a1dd832b3bd658d3e6cf
yoav <yoavdan.m@gmail.com>
2022-10-20 22:29:55 +0300

    fixed overlay bug

--- components/Content.vue
+++ components/Content.vue
@@ -1,66 +1,66 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="about content" id="about">
-    <h2>About.</h2>
-    <p>
-      Hi, I am Yoav Mendelson, a passionate Frontend/FullStack developer.
-      Initially I started Coding out of pure curiosity, it became a hobby and
-      after a while I decided to take the next step and go for it. I’m a recent
-      graduate from the
-      <span>
-        <a href="https://www.coding-academy.org/index.php">
-          Coding Academy
-        </a></span
-      >
-      Bootcamp Where I created some of my projects. Currently I’m searching for
-      an opportunity where i could make an impact. Be sure to contact me if you
-      have any questions of just wanna talk :)
-    </p>
+	<section class="about content" id="about">
+		<h2>About.</h2>
+		<p>
+			Hi, I am Yoav Mendelson, a passionate Frontend/FullStack developer.
+			Initially I started Coding out of pure curiosity, it became a hobby and
+			after a while I decided to take the next step and go for it. I’m a recent
+			graduate from the
+			<span>
+				<a href="https://www.coding-academy.org/index.php">
+					Coding Academy
+				</a></span
+			>
+			Bootcamp Where I created some of my projects. Currently I’m searching for
+			an opportunity where i could make an impact. Be sure to contact me if you
+			have any questions of just wanna talk :)
+		</p>
 
-    <p>
-      I have a genuine passion for development and design, i like building new
-      things that push me out of my comfort zone and challenge my creativity.
-      I'm eager to learn whenever i can, and happy to embrace new ideas. As a
-      developer, my goal is make beautiful websites that not only look good, but
-      also function in multiple levels. I would love to make your digital vision
-      into reality
-    </p>
-  </section>
+		<p>
+			I have a genuine passion for development and design, i like building new
+			things that push me out of my comfort zone and challenge my creativity.
+			I'm eager to learn whenever i can, and happy to embrace new ideas. As a
+			developer, my goal is make beautiful websites that not only look good, but
+			also function in multiple levels. I would love to make your digital vision
+			into reality
+		</p>
+	</section>
 
-  <section class="skills content">
-    <h2 class="skills-title">Skills</h2>
-    <p>
-      I have experience in Vue, Javascript, Typescript, CSS/SASS, HTML, MongoDB,
-      Node.js, Tailwind, Astro , React, and more…  I prefer to learn skills as I
-      need them, so I can focus on finding solutions to problems instead of
-      trying to implement new technologies for the sake of implementing them.
-    </p>
-  </section>
+	<section class="skills content">
+		<h2 class="skills-title">Skills</h2>
+		<p>
+			I have experience in Vue, Javascript, Typescript, CSS/SASS, HTML, MongoDB,
+			Node.js, Tailwind, Astro , React, and more…  I prefer to learn skills as I
+			need them, so I can focus on finding solutions to problems instead of
+			trying to implement new technologies for the sake of implementing them.
+		</p>
+	</section>
 </template>
 
 <style lang="scss">
 .content {
-  h2 {
-    @apply text-3xl font-900 my-6;
-  }
-  p {
-    @apply mb-12;
-    @apply text-base sm:text-xl leading-relaxed text-lime-50;
-    font-weight: 300;
-  }
+	h2 {
+		@apply text-3xl font-900 my-6;
+	}
+	p {
+		@apply mb-12;
+		@apply text-base sm:text-xl leading-relaxed text-lime-50;
+		font-weight: 300;
+	}
 }
 
 .skills {
-  @apply mb-16;
-  .skills-title {
-    @apply font-700 text-2xl;
-  }
+	@apply mb-16;
+	.skills-title {
+		@apply font-700 text-2xl;
+	}
 }
 
 .about {
-  a {
-    @apply text-primary underline underline-offset-4 font-500;
-  }
+	a {
+		@apply text-primary underline underline-offset-4 font-500;
+	}
 }
 </style>
--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -3,146 +3,145 @@ import ArrowUp from '~~/assets/imgs/arrow-up.svg'
 import ArrowUpBlack from '~~/assets/imgs/arrow-up-black.svg'
 
 const props = defineProps<{
-  project: {
-    name: string
-    description: string
-    img: string
-    url: string
-  }
+	project: {
+		name: string
+		description: string
+		img: string
+		url: string
+	}
 }>()
 
 const cardContentShown = ref<Boolean>(false)
 
 function goToProject(url: string) {
-  // if (window.innerWidth < 450) return;
-  console.log('hiii')
-  window.open(url)
+	window.open(url)
 }
 
 function isLightBg(name: string) {
-  // return false;
-  return name === 'Appsus' || name === 'MemeGen' ? true : false
+	return name === 'Appsus' || name === 'MemeGen' ? true : false
 }
 
 function showContent() {
-  cardContentShown.value = !cardContentShown.value
+	cardContentShown.value = !cardContentShown.value
 }
 </script>
 
 <template>
-  <article class="project-card" v-on:click="goToProject(props.project.url)">
-    <div class="overlay hidden md:block"></div>
-    <div v-if="cardContentShown" class="overlay overlay-mobile"></div>
-    <div
-      @click.stop="showContent"
-      class="text"
-      :class="{
-        'translate-y-0': cardContentShown,
-        'translate-y-full': !cardContentShown,
-      }"
-    >
-      <h2
-        :class="{
-          'translate-y-0 ': cardContentShown,
-          'translate-y-[-153%]': !cardContentShown,
-          'bg-lime-400 bg-opacity-50':
-            isLightBg(props.project.name) && !cardContentShown,
-        }"
-        class="flex gap-2 items-center"
-        :style="isLightBg(props.project.name) ? { color: 'black' } : ''"
-      >
-        {{ props.project.name }}
-        <span
-          :class="{ 'rotate-180': cardContentShown }"
-          v-if="props.project.name !== 'More...'"
-        >
-          <img
-            v-if="isLightBg(props.project.name)"
-            :src="ArrowUpBlack"
-            alt="aroow"
-          />
-          <img v-else :src="ArrowUp" alt="" />
-        </span>
-      </h2>
-      <p
-        :class="{
-          'text-slate-100 font-bold': isLightBg(props.project.name),
-        }"
-      >
-        {{ props.project.description }}
-      </p>
-    </div>
-    <img
-      v-if="props.project.img"
-      :src="props.project.img"
-      alt="project image"
-    />
-  </article>
+	<article class="project-card" v-on:click="goToProject(props.project.url)">
+		<div class="overlay"></div>
+		<div v-if="cardContentShown" class="overlay overlay-mobile"></div>
+		<div
+			@click.stop="showContent"
+			class="text"
+			:class="{
+				'translate-y-0': cardContentShown,
+				'translate-y-full': !cardContentShown,
+			}"
+		>
+			<h2
+				:class="{
+					'translate-y-0 ': cardContentShown,
+					'translate-y-[-153%]': !cardContentShown,
+					'bg-lime-400 bg-opacity-50':
+						isLightBg(props.project.name) && !cardContentShown,
+				}"
+				class="flex gap-2 items-center"
+				:style="isLightBg(props.project.name) ? { color: 'black' } : ''"
+			>
+				{{ props.project.name }}
+				<span
+					:class="{ 'rotate-180': cardContentShown }"
+					v-if="props.project.name !== 'More...'"
+				>
+					<img
+						v-if="isLightBg(props.project.name)"
+						:src="ArrowUpBlack"
+						alt="arrow"
+					/>
+					<img v-else :src="ArrowUp" alt="" />
+				</span>
+			</h2>
+			<p
+				:class="{
+					'text-slate-100 font-bold': isLightBg(props.project.name),
+				}"
+			>
+				{{ props.project.description }}
+			</p>
+		</div>
+		<img
+			v-if="props.project.img"
+			:src="props.project.img"
+			alt="project image"
+		/>
+	</article>
 </template>
 
 <style lang="scss">
 .project-card {
-  @apply relative aspect-square overflow-hidden cursor-pointer rounded-lg bg-lime-400 bg-opacity-20;
-  @apply transition-all duration-500;
+	@apply relative aspect-square overflow-hidden cursor-pointer rounded-lg bg-lime-400 bg-opacity-20;
+	@apply transition-all duration-500;
 
-  .overlay {
-    @apply absolute inset-0 bg-gray-900/0 z-0;
-    @apply absolute inset-0 bg-black/20 coarse:bg-[color:rgb(4_23_78/20%)] backdrop-blur-lg coarse:backdrop-blur-sm;
-    @apply opacity-0;
-    @apply transition-opacity duration-500;
+	.overlay {
+		@apply absolute inset-0 bg-gray-900/0 z-0;
+		@apply absolute inset-0 bg-black/20 coarse:bg-[color:rgb(4_23_78/20%)] backdrop-blur-lg coarse:backdrop-blur-sm;
+		@apply opacity-0;
+		@apply transition-opacity duration-500;
+		&:hover {
+			@apply opacity-100;
+		}
+		&.overlay-mobile {
+			@apply opacity-100;
+		}
+	}
 
-    &.overlay-mobile {
-      @apply opacity-100;
-    }
-  }
+	.text {
+		@apply absolute bottom-0 left-0 p-6 z-20;
+		@apply whitespace-normal;
+		@apply transition-transform duration-500;
+		@apply md:translate-y-full;
 
-  .text {
-    @apply absolute bottom-0 left-0 p-6 z-20;
-    @apply whitespace-normal;
-    @apply transition-transform duration-500;
-    @apply sm:translate-y-full;
+		h2 {
+			@apply text-lime-400 font-bold text-3xl py-1;
+			@apply md:translate-y-[-153%];
+			@apply transition duration-500;
 
-    h2 {
-      @apply text-lime-400 font-bold text-3xl py-1;
-      @apply md:translate-y-[-153%];
-      @apply transition duration-500;
+			span {
+				@apply md:hidden transition-transform duration-300;
+				img {
+					@apply w-8;
+				}
+			}
+		}
 
-      span {
-        @apply md:hidden transition-transform duration-300;
-        img {
-          @apply w-8;
-        }
-      }
-    }
+		p {
+			transition-delay: 0.3s;
+		}
+	}
 
-    p {
-      transition-delay: 0.3s;
-    }
-  }
+	img {
+		@apply object-cover h-full w-full rounded-lg;
+	}
 
-  img {
-    @apply object-cover h-full w-full rounded-lg;
-  }
+	@at-root :global(.dom-loaded) .project-card .text {
+		@apply transition-transform duration-500;
+	}
 
-  @at-root :global(.dom-loaded) .project-card .text {
-    @apply transition-transform duration-500;
-  }
+	&:hover {
+		@apply outline outline-2 outline-lime-50;
 
-  &:hover {
-    @apply outline outline-2 outline-lime-50;
+		.text {
+			@apply md:translate-y-0;
+			h2 {
+				// @apply translate-y-0;
 
-    .text {
-      @apply md:translate-y-0;
-      h2 {
-        // @apply translate-y-0;
+				@apply md:translate-y-0 opacity-100;
+			}
+		}
 
-        @apply md:translate-y-0 opacity-100;
-      }
-    }
-
-    .overlay {
-      @apply md:opacity-100;
-    }
-  }
+		.overlay {
+			@apply md:opacity-100;
+		}
+	}
 }
 </style>

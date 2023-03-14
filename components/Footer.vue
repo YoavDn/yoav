ed30124682820d12d5746a37753cd0ff1fb93d58
yoav <yoavdan.m@gmail.com>
2023-03-14 16:36:48 +0200

    changed copyright year

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,39 +5,39 @@ import ArrowSvg from '~/assets/imgs/down-arrow.svg'
 
 
 <template>
-	<div class="footer content" id="contact">
-		<h2>Contact.</h2>
-		<div class="links">
-			<li>
-				<a href="https://www.linkedin.com/in/yoav-mendelson/">
-					LinkedIn <img :src="LinkSvg" alt="" />
-				</a>
-			</li>
-			<li>
-				<a href="https://github.com/YoavDn">
-					Github <img :src="LinkSvg" alt="" />
-				</a>
-			</li>
-			<li>
-				<a href="https://twitter.com/yoav_mendelson">
-					Twitter <img :src="LinkSvg" alt="" />
-				</a>
-			</li>
-			<li>
-				<a href="mailto:yoavdan.m@gmail.com">
-					Email <img :src="LinkSvg" alt="" />
-				</a>
-			</li>
-		</div>
-		<div class="back-to-top">
-			<img :src="ArrowSvg" alt="arrow svg" />
-			<a href="#"> Back to top </a>
-		</div>
+    <div class="footer content" id="contact">
+        <h2>Contact.</h2>
+        <div class="links">
+            <li>
+                <a href="https://www.linkedin.com/in/yoav-mendelson/">
+                    LinkedIn <img :src="LinkSvg" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="https://github.com/YoavDn">
+                    Github <img :src="LinkSvg" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com/yoav_mendelson">
+                    Twitter <img :src="LinkSvg" alt="" />
+                </a>
+            </li>
+            <li>
+                <a href="mailto:yoavdan.m@gmail.com">
+                    Email <img :src="LinkSvg" alt="" />
+                </a>
+            </li>
+        </div>
+        <div class="back-to-top">
+            <img :src="ArrowSvg" alt="arrow svg" />
+            <a href="#"> Back to top </a>
+        </div>
 
-		<h3 class="text-base font-400 text-center mt-10">
-			&copy;2022 Yoav Mendelson
-		</h3>
-	</div>
+        <h3 class="text-base font-400 text-center mt-10">
+            &copy;2023 Yoav Mendelson
+        </h3>
+    </div>
 </template>
 
 
